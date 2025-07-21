d6385a488ff9abe2503f5be86ec9ddec818bab5a
Ivan Semikin <i.semikin.94@icloud.com>
2025-07-21 18:02:43 +0200

    Initial Commit

---
+++ MillionPath/MillionPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillionPathApp.swift
+//  MillionPath
+//
+//  Created by Иван Семикин on 21/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MillionPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
