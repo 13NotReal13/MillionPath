c70e6fbd89ca8300e97ab59e00615ae1bc422285
Sergei Biriukov <mylovelyserge@yandex.ru>
2025-07-23 00:45:28 +0300

    Добавил AudioManager

--- MillionPath/Services/AudioManager.swift
+++ MillionPath/Services/AudioManager.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import AVFoundation
+
+class AudioManager: ObservableObject {
+    private var player: AVAudioPlayer?
+
+    func playSound(named name: String, withExtension ext: String = "mp3") {
+        guard let url = Bundle.main.url(forResource: name, withExtension: ext) else {
+            print("Аудиофайл не найден")
+            return
+        }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print("Ошибка воспроизведения: \(error.localizedDescription)")
+        }
+    }
+    
+    func stop() {
+        player?.stop()
+    }
+}
